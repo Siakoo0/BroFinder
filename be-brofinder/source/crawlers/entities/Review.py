d5f10e4338f9640a1541cb6f31ff759fcc3477a0
siakoo <pannuccioe3@gmail.com>
2023-06-11 22:55:49 +0200

    Aggiunto il parametro media

--- be-brofinder/source/crawlers/entities/Review.py
+++ be-brofinder/source/crawlers/entities/Review.py
@@ -1,3 +1,5 @@
-class Preview:
-    def __init__(self, text, vote=None) -> None:
+from typing import List
+
+class Review:
+    def __init__(self, text : str, vote : int = None, media : List[str] = []) -> None:
         pass
